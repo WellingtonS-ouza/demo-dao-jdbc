95d88b415819ce81de3253da501bd16be53bb329
Wellington Souza <wel.souza88@gmail.com>
2023-03-14 13:31:30 -0300

    [200~findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,17 @@
 package application;
 
-import java.time.LocalDate;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		SellerDao sellerDao = DaoFactory.creatSellerDao();
 		
-		Seller seller  = new Seller(21, "Bob", "bob@email.com", LocalDate.now(), 3000.0, obj);
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 
 	}
